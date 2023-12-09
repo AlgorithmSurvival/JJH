c8baad56364f4cdece64e4ab794b9f3f268049a9
kabbl12 <wogh8615@gmail.com>
2023-12-10 01:58:40 +0900

    [boj] 11382 : boyjungmin

---
+++ boj_steps/1. Input ouput/11382_boyjungmin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long long int num_A, num_B, num_C;
+
+	cin >> num_A >> num_B >> num_C;
+
+	cout << num_A+num_B+num_C << endl;
+
+	return 0;
+}

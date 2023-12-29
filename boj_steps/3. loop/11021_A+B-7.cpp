83db1f38d49218d798930abe4f755deb53848128
kabbl12 <wogh8615@gmail.com>
2023-12-29 16:39:22 +0900

    [boj] 11021: A+B -7

---
+++ boj_steps/3. loop/11021_A+B-7.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void sum_loop(int count){
+	int num1, num2;
+
+	for(int i = 1; i <= count; ++i){
+		cin >> num1 >> num2;
+		cout << "Case #" << i << ": " << num1 + num2 << "\n";
+	}
+}
+
+int main(){
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int count;
+
+	cin >> count;
+
+	sum_loop(count);
+
+	return 0;
+}

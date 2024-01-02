cd86b11fce3e0159298484f108b892badc8b94db
kabbl12 <wogh8615@gmail.com>
2024-01-02 22:50:44 +0900

    [boj] 10810: shoot the ball

---
+++ boj_steps/4. one demensional array/10810_shoot_the_ball.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> count_ball_num(const int N, const int M) {
+	vector<int> basket(N);
+
+	for (int idx = 0; idx < M; ++idx) {
+		int i, j, k;
+		cin >> i >> j >> k;
+
+		for (int bas_idx = i-1; bas_idx < j; ++bas_idx) {
+			basket[bas_idx] = k;
+		}
+	}
+
+	return basket;
+}
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> basket(N) = count_ball_num(N, M);
+
+	for (int cnt = 0; cnt < N; ++cnt)
+		cout << basket[cnt] << " ";
+
+	return 0;
+}

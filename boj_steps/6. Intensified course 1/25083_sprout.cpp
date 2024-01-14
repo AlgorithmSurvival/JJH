be38593e3fba583c5569b933b9f6dd51b0e12d30
kabbl12 <wogh8615@gmail.com>
2024-01-15 00:41:23 +0900

    [boj] 25083: sprout

---
+++ boj_steps/6. Intensified course 1/25083_sprout.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void print_sprout() {
+	cout << "         ,r'\"7" << "\n";
+	cout << "r`-_   ,'  ,/" << "\n";
+	cout << " \\. \". L_r'" << "\n";
+	cout << "   `~\\/" << "\n";
+	cout << "      |" << "\n";
+	cout << "      |" << "\n";
+}
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	print_sprout();
+
+	return 0;
+}

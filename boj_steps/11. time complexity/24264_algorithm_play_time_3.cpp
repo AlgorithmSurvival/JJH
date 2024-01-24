50bb5756a9ee7f57bdc0fac1f7fec257b57d6c58
kabbl12 <wogh8615@gmail.com>
2024-01-24 20:04:37 +0900

    [boj] 11. time complexity

---
+++ boj_steps/11. time complexity/24264_algorithm_play_time_3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+	ll n;
+	cin >> n;
+
+	cout << n * n << "\n" << 2 << "\n";
+
+	return 0;
+}
---
+++ boj_steps/11. time complexity/24265_algorithm_play_time_4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+	ll n;
+	cin >> n;
+
+	cout << n * (n - 1) / 2 << "\n" << 2 << "\n";
+	return 0;
+}

5d17bfa0f55f80301fb7920feb1bc785d40bb86b
kabbl12 <wogh8615@gmail.com>
2024-01-24 19:29:16 +0900

    [boj] 1316: group word checker
    chage if condition
    for >> while
    change mechanism

--- boj_steps/6. Intensified course 1/1316_group_word_checker.cpp
+++ boj_steps/6. Intensified course 1/1316_group_word_checker.cpp
@@ -5,15 +5,27 @@
 using namespace std;
 
 bool is_group_word(const string str) {
-	for (int i = 0; i < str.length(); ++i) {
-		if (i > 0) {
+	/*
+	for (int i = 1; i < str.length(); ++i) {
 			for(int j = 0; j < i; ++j) {
 				if (str[i] != str[i-1] && str[i] == str[j]) {
 					//cout << "0" << "\n";
 					return false;
 				}
 			}
-		}
+	}
+	*/
+	vector<bool> useds('z' + 1);
+
+	int idx = 0;
+	while (idx < str.size()) {
+		//중복사용체크
+		if (useds[str[idx]])
+			return false;
+
+		useds[str[idx]] = true;
+		while ((++idx < str.size())) &&
+			(str[idx - 1] == str[idx]);
 	}
 
 	//cout << "1" << "\n";
